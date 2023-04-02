18ed82ade6582856f8fc492d759e7b6062193b76
Joshua Kinder <joshjkk0@gmail.com>
2023-04-02 19:22:19 +0100

    Start drivers

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,10 +2,13 @@
 #include "../x86/stdlib.h"
 #include "../x86/stdint.h"
 #include "../x86/string.h"
-#include "drivers/keyboard.h"
+#include "drivers/drivers.h"
 
 void kernel_main() {
     set_color(COLOR_WHITE, COLOR_BLACK);
     clear();
+    start_drivers();
+
+    putc('\n');
     log_msg("SYSTEM", "Welcome to JKOS!", COLOR_GREEN);
 }
