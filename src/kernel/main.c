cbe4e7649a4510f5c56a6a4f98eb4979dcc49ade
Joshua Kinder <joshjkk0@gmail.com>
2023-04-02 14:22:01 +0100

    Log welcome

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,6 +1,12 @@
 #include "../x86/stdio.h"
+#include "../x86/stdlib.h"
+#include "../x86/stdint.h"
+#include "../x86/string.h"
+#include "../x86/graphics.h"
+#include "drivers/keyboard.h"
 
 void kernel_main() {
+    set_color(COLOR_WHITE, COLOR_BLACK);
     clear();
-    puts("Welcome to JKOS!\n");
+    log_msg("SYSTEM", "Welcome to JKOS!", COLOR_GREEN);
 }
